35df5948036962d421deb0d1190ee2822df0b1a2
Muzaffar <110671880+M-Orziboyev@users.noreply.github.com>
2023-02-19 20:35:45 +0500

    Update turtile.py

--- turtile.py
+++ turtile.py
@@ -8,7 +8,7 @@ screen = turtle.getscreen()
 # set background color of screen
 screen.bgcolor("white")
 # set tile of screen
-screen.title("USA Flag - https://www.pythoncircle.com")
+screen.title("USA Flag)
 # "Yesterday is history, tomorrow is a mystery, 
 # but today is a gift. That is why it is called the present.”
 # — Oogway to Po, under the peach tree, Kung Fu Panda Movie
